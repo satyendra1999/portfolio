aee2974f84d0cf07042d7738b0daafb660fe6403
Satyendra Rathour <129605001+satyendra1999@users.noreply.github.com>
2024-02-24 16:58:27 +0530

    Add files via upload

---
+++ satyendra.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings:["frontend Developer", " And Programmer" , "Software Engineer"],
+    typeSpeed:40,
+    backSpeed:40,
+    backDelay:500,
+    loop:true
+}); 
